1da5ea10ee9a19b433ee74494936b6cb08b4fa0c
Mustafaali82 <67122730+Mustafaali82@users.noreply.github.com>
2022-07-29 05:29:03 +0500

    file updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./components/navbar";
+import Todoitem from "./components/todoitem";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [todoList, setTodoList] = useState(["Todo App"]);
+  const [message, sentmessage] = useState("");
+
+  const valueChangeHandier = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const addHandler = () => {
+    if (inputValue=="") {
+      return;
+    }
+    setTodoList((prevValue) => [...prevValue, inputValue]);
+    setInputValue("");
+    sentmessage("Successfully Added");
+    setTimeout(() => {
+      sentmessage("");
+    }, 1000);
+
+  };
+
+  const deleteHandler = (name) => {
+    const newList = todoList.filter((item) => item != name);
+    setTodoList(newList);
+    sentmessage("Successfully Deleted");
+    setTimeout(() => {
+      sentmessage("");
+    }, 1000);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+      <div className="container">
+        <div className="inputContainer">
+          <input type="text" placeholder="Enter Your Type" value={inputValue} onChange={valueChangeHandier} />
+          <button onClick={addHandler}>
+            <svg
+              className="svg"
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="#fff"
+              class="bi bi-plus-lg"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"
+              />
+            </svg>
+            Add
+          </button>
+        </div>
+        <p className="a-d">{message}</p>
+        <div className="listContainer">
+          {todoList.map((item) => (
+            <Todoitem
+              key={item}
+              title={item}
+              onDelete={() => {
+                deleteHandler(item);
+              }}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
